81e1c2b39773b6e249e29b4e1e138f66909066dc
Srayan Jana <valorzard.kiko@outlook.com>
2026-09-08 22:03:16 -0700

    add GET and POST

--- examples/http_server_app.adb
+++ examples/http_server_app.adb
@@ -3,14 +3,19 @@ with Iour.Http.Server;
 
 package body Http_Server_App with SPARK_Mode => On is
 
+   use type Iour.Http.Method;
+
    procedure Health
      (Request_Method : Iour.Http.Method;
       Target         : String;
       Payload        : out Iour.Byte_Array;
       Payload_Length : out Natural)
    is
-      pragma Unreferenced (Request_Method, Target);
-      Text : constant String := "ok\n";
+      pragma Unreferenced (Target);
+      Text : constant String :=
+        (if Request_Method = Iour.Http.Get then "GET ok" & ASCII.LF
+         elsif Request_Method = Iour.Http.Post then "POST ok" & ASCII.LF
+         else "method not supported" & ASCII.LF);
    begin
       Payload := [others => 0];
       for Index in Text'Range loop
